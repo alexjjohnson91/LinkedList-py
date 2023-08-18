6ba6a91b4455facbdd76ec4d144fd45bb2ef50e1
Alex <alexjjohnson91@gmail.com>
2023-08-18 11:52:47 -0400

    templating list class

--- linked-list.py
+++ linked-list.py
@@ -1,15 +1,51 @@
 
 class Node:
     # constructor
-    def __init__(self, data = None, next = None):
-        self.data = data
+    def __init__(self, value = None, next = None):
+        self.value = value
         self.next = next
 
 # A Linked List class with a single head node
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.size = 0
 
-    
+    # print the contents of the list
+    def print(self) -> None:
+        pass
+
+    # push to the front of the list
+    def push_front(self, val) -> None:
+        pass
+
+    # append to the end of the list
+    def append(self, val) -> None:
+        pass
+
+    # return the size of the list
+    def length(self) -> None:
+        pass
+
+    # empty out the list
+    def clear(self) -> None:
+        pass
+
+    # delete element i of the list
+    def remove(self, index) -> None:
+        pass
+
+    # get element at index i
+    def get_element(self, index) -> None:
+        pass
+
+    # insert element into position at index i
+    def insert(self, index) -> None:
+        pass
+
+    # replace element at position i of the list
+    def replace(self, index) -> None:
+        pass
+
 
 
