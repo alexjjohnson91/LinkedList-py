9fccf72cd147364a7abdd22078d37bb6b1c676ef
Alex <alexjjohnson91@gmail.com>
2023-08-23 11:07:03 -0400

    condense redundant code in test setup

--- test_list.py
+++ test_list.py
@@ -8,6 +8,11 @@ class TestList(unittest.TestCase):
             Set up the test fixtures
         """
         self.a = linked_list.LinkedList()
+        self.a.append(1)
+        self.a.append(2)
+        self.a.append(3)
+        self.a.append(4)
+        self.a.append(5)
     
     def tearDown(self):
         """
@@ -23,30 +28,18 @@ class TestList(unittest.TestCase):
         
 
     def test_append(self):
-        self.a.append(5)
-        self.assertEqual(self.a.head.value, 5)
-        self.a.append(4)
-        self.assertEqual(self.a.head.value, 5)
-        self.assertEqual(self.a.size, 2)
+        self.a.append(6)
+        self.assertEqual(self.a.size, 6)
+        self.a.append(7)
+        self.assertEqual(self.a.size, 7)
+
 
     def test_length(self):
-        self.a.append(1)
-        self.a.append(2)
-        self.a.append(3)
-        self.a.append(4)
-        self.a.append(5)
-
         self.assertEqual(self.a.length(), 5)
         self.a.push_front(0)
         self.assertEqual(self.a.length(), 6)
 
     def test_clear(self):
-        self.a.append(1)
-        self.a.append(2)
-        self.a.append(3)
-        self.a.append(4)
-        self.a.append(5)
-
         self.a.clear()
         self.assertEqual(self.a.size, 0, "testing clear")
         self.assertEqual(self.a.head.get_value(), None)
